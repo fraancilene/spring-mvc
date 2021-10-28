ebddc4672f273a91ee15bfbc8ba34438182dd239
Francilene Silva <francilenesilva.fps10@gmail.com>
2021-10-28 11:59:47 +0100

    Iniciando o projeto

---
+++ src/main/java/br/com/alura/mvc/mudi/MudiApplication.java
@@ -0,0 +1,18 @@
+package br.com.alura.mvc.mudi;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class MudiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MudiApplication.class, args);
+    }
+
+}
